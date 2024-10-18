7cb15375420fc2dec70d5d44fa134fd56b24cc85
Ayush <softprotechcoader@outlook.com>
2024-10-18 13:43:07 +0530

    facing issue with auth guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,13 @@
+// import { authGuard } from './auth.guard';
+import { Injectable } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export const authGuard: CanActivateFn = (route, state) => {
+//   return true;
+// };
+// export class authGuard implements CanActivateFn{
+
+// }
