1a7bc105fbaf847f77650765abfb28633fcc20c9
Ayush <softprotechcoader@outlook.com>
2024-10-18 13:03:48 +0530

    created routing path with authGuard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { LayoutComponent } from './Pages/layout/layout.component';
+import { authGuard } from './auth.guard';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'login',pathMatch:'full'
+    },
+    {
+        path:'login',component:LoginComponent
+    },
+    {
+        path:'',component:LayoutComponent, canActivate: [authGuard],
+        children:[
+            {
+                path:'dashboard',component:DashboardComponent
+            }
+        ]
+        
+    },
+    {path:'**',redirectTo:'login'}
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule{
+    
+}
