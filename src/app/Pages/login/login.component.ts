3fa0cbf16d8f8f135207f80cea0582198f57ebe6
Ayush <softprotechcoader@outlook.com>
2024-10-18 14:39:33 +0530

    getting blank screen

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.login({ username: this.username, password: this.password })
+      .subscribe(response => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['dashboard']);
+      });
+  }
 }
